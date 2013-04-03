d5df84186e9e2eb65aec4d2ec10d39bbcf395da8
David Jacobs <david@wit.io>
2013-04-02 22:56:46 -0700

    add lein outdated

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,9 @@
 {:user {:plugins [[lein-midje "2.0.4"]
                   [lein-tarsier "0.10.0"]
                   [lein-immutant "0.14.1"]
-                  [lein-iclojure "1.1"]]
-        :dependencies {clj-stacktrace "0.2.4"}
+                  [lein-iclojure "1.1"]
+                  [lein-outdated "1.0.0"]]
+        :dependencies {clj-stacktrace "0.2.5"}
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
