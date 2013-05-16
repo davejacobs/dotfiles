f8b6265bbc3ccac7a5b3e654689c59bb9b07355e
David Jacobs <david@wit.io>
2013-05-15 22:54:05 -0700

    remove old plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,4 @@
-{:user {:plugins [[lein-midje "3.0.0"]
-                  [lein-immutant "0.14.1"]
-                  [lein-iclojure "1.1"]
-                  [lein-outdated "1.0.0"]]
+{:user {:plugins []
         :dependencies {clj-stacktrace "0.2.5"}
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
