b531b932ccc1eb63b92c2b5620badede76853b50
David Jacobs <david@wit.io>
2015-04-23 19:34:44 -0700

    Remove lein config for the moment.

--- lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:plugins []
-        :dependencies {clj-stacktrace "0.2.5"}
-        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly @new)))]}}
